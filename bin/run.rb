a15f42537fbe66979183cf84c559842964e26298
Javi <javsteratii@gmail.com>
2019-12-06 07:50:56 -0500

    CLI REMEMBERS USERANAME FOR ALL OTHER METHODS

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,4 @@
 require_relative '../config/environment'
 cli = CommandLineInterface.new
+
 cli.run
--- lib/CLI.rb
+++ lib/CLI.rb
@@ -2,12 +2,13 @@ require "tty-prompt"
 require "tty-spinner"
 require 'pry'
 
-class CommandLineInterface
+class CommandLineInterface 
+    attr_reader :currentuser
 
     def run 
         greet
-    end
-
+    end 
+ 
     def greet
         font = TTY::Font.new(:straight)
         5.times do 
@@ -26,69 +27,84 @@ class CommandLineInterface
         prompt = TTY::Prompt.new
         puts "====\n====\n"
         prompt.select("create an account or sign in") do |menu|
-        menu.choice 'SIGN IN', -> {home}
+        menu.choice 'SIGN IN', -> {login}
         menu.choice 'CREATE AN ACCOUNT', -> {create_account}
         #use proc to send to the right methods
         end 
     end 
 
-    def home 
+    def login 
         prompt = TTY::Prompt.new
         #CHECKS DATA BASE AND IF USER AND PASS MATCH TAKE TO HOME PAGE
         puts "Welcome back to Rover(ish)!\n"
         puts "ENTER USERNAME BELOW:"
         input_username = gets.chomp.downcase!
         password_input = prompt.mask("\nENTER PASSWORD BELOW:")
-        user = find_user(input_username).password
-            if user != password_input
+        checkuser = find_user(input_username).password
+            until checkuser == password_input
                 puts "NO ACCOUNT FOUND WITH THAT USERNAME OR PASSWORD"
                 puts "\nENTER USERNAME BELOW:"
                 input_username = gets.chomp.downcase!
                 password_input = prompt.mask("\nENTER PASSWORD BELOW:")
             end
-        current_user = input_username
-        prompt.select("Welcome back #{find_user(input_username).name.first}!\n<><> |  ROVER(ish) |  <><>") do |menu|
-            40.times do 
-                    puts "|||||||||||||"
-            end 
-        puts "LOG IN SUCCESSFUL\n \n"
-                menu.choice 'Walk a Dog', -> {cr}
-                menu.choice 'View My Balance', -> {greet}
-                menu.choice 'Withdraw Balance', -> {greet}
-                menu.choice 'Past Orders', -> {greet}
-                menu.choice 'ACCOUNT SETTINGS', -> {greet}
-            #acc settings does change pass, name and delete account
-        end 
-    
-        def walk_order
-            #smple a random dog by ID creates a order with current user and chosen dog. 
-            #ALSO UPDATES USERS BALANCE BY ADDING AN ADDITIONAL $14 TO CURRENT AMOUNT
-
-        end
-
-        def user_balance
-            #ACTIVER SEARCHES FOR USER IN DB AND RETURNS THE BALANCE COLUMN VALUE.
-        end 
-
-        def delete_account
-            #user can remove all values of itself in DB
-        end 
-
-        def change_password
-            #USER CAN CHANGE PASSWORD
-        end 
-
-        def withdrawl
-            #USER CAN WITHDRAW AND DELETE BALANCE FROM DB
-        end 
-
-        def past_walks
-            #USER SEARCHES IN DATABASE FOR ALL ORDERS WITH THEIR ID, RETURNS DATE, DOG NAME AND HOW MUCH.
-        end 
-
-        def account_settings
-        end 
+        @currentuser = find_user(input_username)
+        home
     end 
+
+
+    def home
+            prompt = TTY::Prompt.new
+            prompt.select("Welcome back #{@currentuser.name.split.first}!\n<><> |  ROVER(ish) |  <><>") do |menu|
+            40.times do 
+                puts "|||||||||||||"
+            end 
+            puts "LOG IN SUCCESSFUL\n \n"
+                    menu.choice 'Walk a Dog', -> {walk_order}
+                    menu.choice 'View My Balance', -> {greet}
+                    menu.choice 'Withdraw Balance', -> {greet}
+                    menu.choice 'Past Orders', -> {greet}
+                    menu.choice 'ACCOUNT SETTINGS', -> {greet}
+                #acc settings does change pass, name and delete account
+            end
+
+    end 
+        
+            def walk_order
+                puts "NOW CHECKING FOR NEAREST DOG IN AREA IN NEED OF A WALK."
+                walk = Order.create(dog_id: Dog.all.sample.id, user_id: @currentuser.id,  datetime: DateTime.now, earnings: 14.0)
+                findname = walk.dog_id
+                dogname = Dog.find_by(id: findname).name
+                puts "FOUND #{dogname}! She is ready for her walk now!" 
+                puts "$14.00 Was added to your balance #{@currentuser.username}!"
+                puts "+++++++++++++\nRETURNING TO LOGIN PAGE"
+                10.times do 
+                    puts "|||||||||||||||||||||||"
+                end 
+                login
+            end
+
+            def user_balance
+                #ACTIVER SEARCHES FOR USER IN DB AND RETURNS THE BALANCE COLUMN VALUE.
+            end 
+
+            def delete_account
+                @currentuser.destroy
+            end 
+
+            def change_password
+                #USER CAN CHANGE PASSWORD
+            end 
+
+            def withdrawl
+                #USER CAN WITHDRAW AND DELETE BALANCE FROM DB
+            end 
+
+            def past_walks
+                #USER SEARCHES IN DATABASE FOR ALL ORDERS WITH THEIR ID, RETURNS DATE, DOG NAME AND HOW MUCH.
+            end 
+
+            def account_settings
+            end 
 
     def create_account
         prompt = TTY::Prompt.new
@@ -127,7 +143,6 @@ class CommandLineInterface
         puts "WE ARE NOW SENDING YOU TO THE LOG IN SECTION."
         5.times do 
             "==<>=="
-        end
-        home
+         end
     end 
-end 
+end
